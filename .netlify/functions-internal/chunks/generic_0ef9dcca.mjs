c001c7d9539baea351557a0915a4d79310795c9d
Santos Alarcón <santosalarcon86@gmail.com>
2024-11-16 13:05:24 +0100

    Recreated netlify folder

--- .netlify/functions-internal/chunks/generic_0ef9dcca.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_e710ad22.mjs').then(n => n.g);
-
-export { page };
--- .netlify/functions-internal/chunks/prerender_339eac73.mjs
+++
@@ -1,4 +0,0 @@
-const noop = () => {};
-export const V = noop;
-export const _ = noop;
-export const i = noop;
